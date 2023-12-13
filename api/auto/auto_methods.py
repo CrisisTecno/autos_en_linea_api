b54cdfda2a1c42ab881ede0d1292b016fc0c65c0
Cristhian-L-Q-N <tecn0crisis0@gmail.com>
2023-12-13 09:37:34 -0300

    create methods put's and delete's

--- api/auto/auto_methods.py
+++ api/auto/auto_methods.py
@@ -43,3 +43,51 @@ async def crear_auto():
 
     except Exception as e:
         return jsonify({"error": f"Error en la base de datos: {e}"}), 500
+
+@auto_fl2.route('/auto/<int:id_auto>', methods=['PUT'])
+async def actualizar_auto(id_auto):
+    try:
+        async with connect_to_database() as connection:
+            data = request.json
+            campos_permitidos = ['nombre', 'descripcion', 'disponibilidad', 'categoria_de_auto',
+                                 'marca', 'modelo', 'anio', 'kilometraje', 'precio', 'especificaciones']
+            
+            if not any(campo in data for campo in campos_permitidos):
+                return jsonify({"error": "Se requiere al menos un campo para actualizar"}), 400
+                
+            async with connection.cursor() as cursor:
+                sql_update = "UPDATE auto SET "
+                valores = []
+
+                for campo in campos_permitidos:
+                    if campo in data:
+                        sql_update += f"{campo} = %s, "
+                        valores.append(data[campo])
+
+                sql_update = sql_update.rstrip(', ')
+
+                sql_update += " WHERE id_auto = %s"
+                valores.append(id_auto)
+
+                await cursor.execute(sql_update, valores)
+                await connection.commit()
+
+            return jsonify({"success": True, "message": f"Auto con ID {id_auto} actualizado exitosamente"}), 200
+
+    except Exception as e:
+        return jsonify({"error": f"Error en la base de datos: {e}"}), 500
+
+
+@auto_fl2.route('/auto/<int:id_auto>', methods=['DELETE'])
+async def eliminar_auto(id_auto):
+    try:
+        async with connect_to_database() as connection:
+            async with connection.cursor() as cursor:
+                sql_delete = "DELETE FROM auto WHERE id_auto = %s"
+                await cursor.execute(sql_delete, (id_auto,))
+                await connection.commit()
+
+            return jsonify({"success": True, "message": f"Auto con ID {id_auto} eliminado exitosamente"}), 200
+
+    except Exception as e:
+        return jsonify({"error": f"Error en la base de datos: {e}"}), 500
--- api/usuario/usuario_methods.py
+++ api/usuario/usuario_methods.py
@@ -39,3 +39,52 @@ async def crear_usuario():
 
     except Exception as e:
         return jsonify({"error": f"Error en la base de datos: {e}"}), 500
+
+
+@usuario_fl2.route('/usuario/<int:id_usuario>', methods=['PUT'])
+async def actualizar_usuario(id_usuario):
+    try:
+        async with connect_to_database() as connection:
+            data = request.json
+            campos_permitidos = ['contrasena', 'rol', 'nombres', 'apellidos', 'correo_electronico',
+                                 'contacto', 'direccion', 'num_telefono', 'url_logo']
+            
+            if not any(campo in data for campo in campos_permitidos):
+                return jsonify({"error": "Se requiere al menos un campo para actualizar"}), 400
+                
+            async with connection.cursor() as cursor:
+                sql_update = "UPDATE usuario SET "
+                valores = []
+
+                for campo in campos_permitidos:
+                    if campo in data:
+                        sql_update += f"{campo} = %s, "
+                        valores.append(data[campo])
+
+                sql_update = sql_update.rstrip(', ')
+
+                sql_update += " WHERE id_usuario = %s"
+                valores.append(id_usuario)
+
+                await cursor.execute(sql_update, valores)
+                await connection.commit()
+
+            return jsonify({"success": True, "message": f"Usuario con ID {id_usuario} actualizado exitosamente"}), 200
+
+    except Exception as e:
+        return jsonify({"error": f"Error en la base de datos: {e}"}), 500
+
+
+@usuario_fl2.route('/usuario/<int:id_usuario>', methods=['DELETE'])
+async def eliminar_usuario(id_usuario):
+    try:
+        async with connect_to_database() as connection:
+            async with connection.cursor() as cursor:
+                sql_delete = "DELETE FROM usuario WHERE id_usuario = %s"
+                await cursor.execute(sql_delete, (id_usuario,))
+                await connection.commit()
+
+            return jsonify({"success": True, "message": f"Usuario con ID {id_usuario} eliminado exitosamente"}), 200
+
+    except Exception as e:
+        return jsonify({"error": f"Error en la base de datos: {e}"}), 500
